f22c1573c01575c557b77b87bdb5ce3ca907f379
Laksh <laksh@genei.io>
2020-05-25 16:42:46 +0100

    Bump version: 0.1.8 → 0.1.9

--- sparsemax/__init__.py
+++ sparsemax/__init__.py
@@ -2,7 +2,7 @@
 # fmt: off
 __author__ = """Laksh Aithani"""
 __email__ = "lakshaithanii@gmail.com"
-__version__ = '0.1.8'
+__version__ = '0.1.9'
 
 from sparsemax.sparsemax import Sparsemax
 # fmt: on
